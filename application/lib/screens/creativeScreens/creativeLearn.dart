78f6c75c42eef5c40d1fa95728b3141979faaf0c
JoshuaBiswas <joshuabiswas229@gmail.com>
2023-10-24 14:28:33 -0400

    Add embeded youtube video as example to learn page

--- application/lib/screens/creativeScreens/creativeLearn.dart
+++ application/lib/screens/creativeScreens/creativeLearn.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class CreativeLearn extends StatelessWidget {
+class CreativeLearn extends StatefulWidget {
   const CreativeLearn({super.key});
 
   @override
+  State<CreativeLearn> createState() => _CreativeLearnState();
+}
+
+class _CreativeLearnState extends State<CreativeLearn> {
+  final videoURL = "https://www.youtube.com/watch?v=YMx8Bbev6T4";
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    final videoID = YoutubePlayer.convertUrlToId(videoURL);
+    _controller = YoutubePlayerController(
+        initialVideoId: videoID!,
+        flags: const YoutubePlayerFlags(autoPlay: false));
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("Let us learn!"));
+    return Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: YoutubePlayer(
+            controller: _controller,
+            showVideoProgressIndicator: true,
+            onReady: () => debugPrint("Ready to play the youtube video!"),
+          ),
+        )
+      ],
+    ));
   }
 }
